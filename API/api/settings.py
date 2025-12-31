5cb6f12d4fc9fb055b738275d175dfdea3f82541
Hanniel Degbelo <114691127+Hanniel123@users.noreply.github.com>
2025-12-31 23:30:52 +0100

    Update settings.py

--- API/api/settings.py
+++ API/api/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 import environ
-
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,16 +118,12 @@ CACHES = {
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'stm',
-        'USER': 'stm_user',
-        'PASSWORD': env('DATABASE_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
-
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
+} 
 # Cors
 CORS_ALLOW_ALL_ORIGINS = True
 
