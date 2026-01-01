e4292659897d6bfe4c9363caa6d838ab44b69e55
JosiasDH <hanniel.degbelo@epitech.eu>
2026-01-01 23:52:27 +0100

    feat: Update ALLOWED_HOSTS to include production URL for deployment

--- API/api/settings.py
+++ API/api/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-ble*n$!zefaxfdp8b62(&65fi!d5&pvop=hgdpenk^ub%d4z9c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG", default=True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://studentmission.onrender.com']
 
 # Media
 # Media
